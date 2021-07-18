d5edfc09a722bbf25fc0185d3145d1528dffa043
kawamataryou <ba068082@gmail.com>
2021-07-18 17:01:32 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# User Scripts
+My user scripts.
+
+# Stop seeing infinitely
+
+User script to hide recommendations and more in YouTube watch page.
+
+
---
+++ src/stop-seeing-infinitely.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         Stop seeing infinitely
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  try to take over the world!
+// @author       You
+// @match https://www.youtube.com/watch?v=*
+// @grant        none
+// @icon https://www.youtube.com/s/desktop/a910c60b/img/favicon_96x96.png
+// @updateURL    https://github.com/kawamataryo/userscripts/raw/master/src/stop-seeing-infinitely.user.js
+// @downloadURL  https://github.com/kawamataryo/userscripts/raw/master/src/stop-seeing-infinitely.user.js
+// ==/UserScript==
+
+const toDisplayNone = (el) => {
+  el.style.display = "none";
+};
+
+(async function () {
+  console.log("Enabled stop seeing infinitely.");
+
+  // Stop next video when video ended
+  const video = document.querySelector(
+    "#movie_player > div.html5-video-container > video"
+  );
+  if (video) {
+    video.addEventListener("ended", () => {
+      const cancelBtn = document.querySelector(
+        ".ytp-autonav-endscreen-button-container > button"
+      );
+      if (cancelBtn) {
+        cancelBtn.click();
+      }
+
+      const endedContent = document.querySelector(".ytp-endscreen-content");
+      if (endedContent) {
+        toDisplayNone(endedContent);
+      }
+    });
+  }
+
+  // Hide sidebar
+  const primary = document.querySelector("#primary");
+  const sidebar = document.querySelector("#secondary");
+  if (primary && sidebar) {
+    primary.style.maxWidth = `${primary.clientWidth}px`;
+    toDisplayNone(sidebar);
+  }
+
+  // Hide meta
+  const meta = document.querySelector("#meta");
+  if (meta) {
+    toDisplayNone(meta);
+  }
+
+  // Hide comments
+  const comments = document.querySelectorAll("#comments");
+  if (comments) {
+    comments.forEach((e) => {
+      toDisplayNone(e);
+    });
+  }
+})();
